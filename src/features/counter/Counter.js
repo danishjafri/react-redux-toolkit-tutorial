2d82f9a7ba2c04f2ea69377e4ccc9ca01fd2e6d0
Danish Jafri <danishjafri@outlook.com>
2022-07-18 07:54:27 +0100

    Completed a complicated setup for redux.

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,7 +1,16 @@
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./counterSlice";
+import { increment, decrement, reset, incrementByAmount } from "./counterSlice";
+import { useState } from "react";
+
 const Counter = () => {
     const count = useSelector((state) => state.counter.count);
+    const [incrementAmount, setIncrementAmount] = useState(0);
+    const addValue = Number(incrementAmount) || 0; // this will prevent NaN values
+    const resetAll = () => {
+        setIncrementAmount(0);
+        dispatch(reset());
+    }
+
     const dispatch = useDispatch();
     return (
         <section>
@@ -9,6 +18,11 @@ const Counter = () => {
             <div>
                 <button onClick={() => dispatch(increment())}>+</button>
                 <button onClick={() => dispatch(decrement())}>-</button>
+            </div>
+            <input type="text" value={incrementAmount} onChange={(e) => setIncrementAmount(e.target.value)} />
+            <div>
+                <button onClick={() => dispatch(incrementByAmount(addValue))}>Add Amount</button>
+                <button onClick={resetAll}>Reset</button>
             </div>
         </section>
     );
